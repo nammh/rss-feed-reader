e7edc8587deae4cec11a6d3c1f42498b6bf14ed0
Moohyeon Nam <moohyeon.nam@gmail.com>
2017-12-28 18:33:43 +0900

    Modified the behavior according to the previous modification

--- add_feed.py
+++ add_feed.py
@@ -1,8 +1,9 @@
 import sys
 from bs4 import BeautifulSoup
 import web_handler
-import db_handler
 import opml_handler
+import db_handler
+import sqlite3
 
 if __name__=="__main__":
     if len(sys.argv) < 2:
@@ -10,16 +11,22 @@ if __name__=="__main__":
         sys.exit()
 
     feedlist = []
-    if sys.argv[1].find(".opml"):
+    if ".opml" in sys.argv[1]:
         feedlist += opml_handler.parse(sys.argv[1])
         for item in feedlist:
             db_handler.add_source(item)
     else:
         with open(sys.argv[1], 'r', encoding='utf-8') as ifs:
             for line in ifs:
-                feedlist.append({"title":"", "url":line, "type":"rss"});
+                feedlist.append({"title":"", "url":line});
 
         for item in feedlist:
             soup = BeautifulSoup(web_handler.get(item["url"]), "xml")
             item["title"] = soup.channel.title.text
-            db_handler.add_source(item)
+            conn = sqlite3.connect('feed.db')
+            cur = conn.cursor()
+            cur.execute("insert into feed (title, url) values (?, ?)",
+                    (item["title"], item["url"]))
+            db_handler.update_source(cur, item["url"])
+            conn.commit()
+            conn.close()
